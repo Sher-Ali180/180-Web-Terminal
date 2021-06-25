5b1d7fb093e800ed4bbc5fc9cbf4e925342e7cc0
Abeerahmad566 <sherbat60@gmail.com>
2021-06-25 18:59:36 +0500

    hello

---
+++ frontend/src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Banner from "../Banner";
+import Nav from "../Nav";
+
+function Home() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <Banner />
+    </div>
+  );
+}
+
+export default Home;
